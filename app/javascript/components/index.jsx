b38179009dc2a8662065d0bc2d2bbf39e70de38a
faizaankhan3 <faizaan.open@gmail.com>
2024-03-04 02:34:27 +0530

    first commit

---
+++ app/javascript/components/index.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client"
+import App from "./App";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<App />);
+});
